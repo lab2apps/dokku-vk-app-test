018a8bbb6ee926e169421f70700b4584e590ebd5
Galnykin Dmitriy <dmitry.galnykin@clausematch.com>
2018-11-08 21:41:54 +0300

    [test commit] removed service worker

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
 import App from './App';
-import registerServiceWorker from './sw';
 
 // Init VK App
 connect.send('VKWebAppInit', {});
-
-// Service Worker For Cache
-registerServiceWorker();
 
 ReactDOM.render(<App />, document.getElementById('root'));
